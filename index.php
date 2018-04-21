b91e78acbf5d119909c0a0ccd9d2d8f3a5f2475d
小倉 且也 <wm.iidx@gmail.com>
2018-04-21 23:50:55 +0900

    時間の差分表示やラベルの日本語化など

--- index.php
+++ index.php
@@ -9,31 +9,46 @@ if (empty($_SESSION['state']) || $_SESSION['state'] != 'login')
 
 // データベースの接続
 include_once('./db-connection.php');
+include_once('./util.php');
 ?>
 <html>
 <head>
 	<meta http-equiv="content-language" content="ja">
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+	<meta name="viewport" content="width=device-width,maximum-scale=1">
     <title>生活RTAシステム - トップページ</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
 <head>
 <body>
+<div id="header">
+生活RTAシステム
+</div>
 <?php
 // データの挿入
 try
 {
-	$sql = 'SELECT * FROM `daily-rta` WHERE state = :state';
+	$sql = 'SELECT * FROM `daily-rta` WHERE time < :time';
 	$sth = $pdo->prepare($sql);
-	$sth->bindValue(':state',  'start');
+	$sth->bindValue(':time',  '2018-04-22');
     $sth->execute();
 }
 catch(PDOException $e)
 {
 	exit();
 }
+
+echo '<table>';
+$old = 0;
 while ($row = $sth->fetch(PDO::FETCH_ASSOC))
 {
-    echo '> ' . $row['label'] . ' - ' . $row['state'] . '<br>';
+    echo '<tr>';
+    echo '<td>' . sec2time(strtotime($row['time']) - $old) . '</td>';
+    echo '<td>' . label2JPN($row['label']) . '</td>';
+    echo '<td>' . $row['state'] . '</td>';
+    echo '</tr>';
+    $old = strtotime($row['time']);
 }
+echo '</table>';
 ?>
 </body>
 </html>
---
+++ util.php
@@ -0,0 +1,45 @@
+<?php
+function label2JPN ($label)
+{
+    $label_jp = '';
+
+    switch ($label)
+    {
+        case 'bed':
+            $label_jp = 'ベッド';
+            break;
+        case 'dish':
+            $label_jp = '食事';
+            break;
+        case 'bath':
+            $label_jp = '入浴';
+            break;
+        case 'out':
+            $label_jp = '外出';
+            break;
+        case 'desk':
+            $label_jp = '机';
+            break;
+        default:
+            $label_jp = 'その他';
+    }
+    return $label_jp;
+}
+
+function sec2time ($second)
+{
+    $time = '';
+    if ($second >= 3600)
+    {
+        $time = ($second - ($second % 3600)) / 3600 . '時間';
+        $second = $second % 3600;
+    }
+    if ($second >= 60)
+    {
+        $time .= ($second - ($second % 60)) / 60 . '分';
+        $second = $second % 60;
+    }
+    $time .= $second . '秒';
+
+    return $time;
+}
