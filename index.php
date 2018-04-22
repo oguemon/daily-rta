387debb559ee5639fc94dd19fde89db873b4ea64
小倉 且也 <wm.iidx@gmail.com>
2018-04-22 13:38:40 +0900

    掲載順序変更＆時間の差を正しく求める

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ for ($backday = 0; $backday < 3; $backday++)
 
     try
     {
-        $sql = 'SELECT * FROM `daily-rta` WHERE time >= :start AND time < :end';
+        $sql = 'SELECT * FROM `daily-rta` WHERE time >= :start AND time < :end ORDER BY `id` DESC';
         $sth = $pdo->prepare($sql);
         $sth->bindValue(':start', date('Y-m-d', $day_pt));
         $sth->bindValue(':end',   date('Y-m-d', $day_pt + 86400));
@@ -49,25 +49,53 @@ for ($backday = 0; $backday < 3; $backday++)
     
     $old = array('time' => 0,'label' => '');
 
-    foreach ($result as $row)
+    for ($n = 0; $n < count($result); $n++)
     {
+        $d = $result[$n];
+
         echo '<tr>';
-        echo '<td>' . date('H:i.s', strtotime($row['time'])) . '</td>';
-        echo '<td>' . label2JPN($row['label']) . '</td>';
-        echo '<td>' . state2JPN($row['state']) . '</td>';
-        echo '</tr>';
-        if ($row['state'] == 'start')
+        echo '<td>' . date('H:i.s', strtotime($d['time'])) . '</td>';
+        echo '<td>' . state2JPN($d['state']) . '</td>';
+        
+        if ($d['state'] == 'end')
         {
-            $old['time']  = strtotime($row['time']);
-            $old['label'] = $row['label'];
-        }
-        else
-        {
-            if ($old['label'] == $row['label'])
+            // 末端なら
+            if ($n == count($result) - 1)
             {
-                echo '<td colspan="3">' . sec2time(strtotime($row['time']) - $old['time']) . '</td>';
+
+                echo '<td>' . label2JPN($d['label']) . '</td>';
+                echo '<td>' . sec2time(strtotime($d['time']) - strtotime(date('Y-m-d 00:00:00', $day_pt))) . '</td>';
+            }
+            //末端でない
+            else
+            {
+                $nextd = $result[$n + 1];
+                if($nextd['label'] == $d['label'])
+                {
+                    echo '<td rowspan="2">' . label2JPN($d['label']) . '</td>';
+                }
+                else
+                {
+                    echo '<td rowspan="2">' . label2JPN($nextd['label']) . '</td>';
+                }
+                echo '<td rowspan="2">' . sec2time(strtotime($d['time']) - strtotime($nextd['time'])) . '</td>';
             }
         }
+        else
+        if ($d['state'] == 'start' && $n == 0)
+        {
+            echo '<td>' . label2JPN($d['label']) . '</td>';
+            if (date('Y-m-d', $day_pt) == date('Y-m-d'))
+            {
+                echo '<td>' . sec2time(time() - strtotime($d['time'])) . '</td>';
+            }
+            else
+            {
+                echo '<td>' . sec2time(strtotime(date('Y-m-d', strtotime($d['time']))) + 86400 - strtotime($d['time'])) . '</td>';
+            }
+        }
+        
+        echo '</tr>';
     }
     echo '</table>';
 }
